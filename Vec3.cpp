40bad2af387746c85d6c581b740297fab5fe1c70
Sebastian-V-Apply-Digital <sebastian.villarroel@applydigital.com>
2025-04-03 11:55:14 -0400

    feat: add vec3 class

---
+++ Vec3.cpp
@@ -0,0 +1,18 @@
+﻿#include "Vec3.h"
+
+void Vec3::Set(const float x_, const float y_, const float z_)
+{
+    x = x_;
+    y = y_;
+    z = z_;
+}
+
+Vec3::Vec3()
+{
+    Set(0.0f, 0.0f, 0.0f);
+}
+
+Vec3::Vec3(const float x, const float y, const float z)
+{
+    Set(x, y, z);
+}
---
+++ Vec3.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+class Vec3
+{
+public:
+    float x, y, z;
+    void Set(float x, float y, float z);
+
+    Vec3();
+    Vec3(float x, float y, float z);
+};
